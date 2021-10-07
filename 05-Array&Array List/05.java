f0a37e71c0c26efcd80f289222c2b8d42a555422
Ayas Ahmad <ayasahmad63@gmail.com>
2021-10-07 19:36:54 +0530

    Array

--- 05-Array&Array List/05.java
+++ 05-Array&Array List/05.java
@@ -1,14 +1,5 @@
-import java.util.*;
-
-public class Q_05_ShuffleTheArray {
-
-    public static void main(String[] args) {
-        int[] nums={2,5,1,3,4,7};
-        int n=3;
-        int[] ShuffleArray=ShuffleArray(nums,n);
-        System.out.println(Arrays.toString(ShuffleArray));
-    }
-    static int[] ShuffleArray(int[] nums, int n){
+class Solution {
+    public int[] shuffle(int[] nums, int n) {
         int[] ShuffleArray=new int[2*n];
         for(int i=0;i<2*n;i++)
         {
