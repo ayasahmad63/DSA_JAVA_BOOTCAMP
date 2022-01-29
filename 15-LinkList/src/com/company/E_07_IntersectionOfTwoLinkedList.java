f183227769c818d6c4c246b7733f60dc39552c48
Ayas Ahmad <ayasahmad63@gmail.com>
2022-01-29 23:13:30 +0530

    LinkedList

---
+++ 15-LinkList/src/com/company/E_07_IntersectionOfTwoLinkedList.java
@@ -0,0 +1,48 @@
+package com.company;
+
+public class E_07_IntersectionOfTwoLinkedList {
+    public static void main(String[] args) {
+
+
+
+//        public class Solution {
+//            public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+//                if(headA==headB){
+//                    return headA;
+//                }
+//
+//                ListNode temp1=headA;
+//                ListNode temp2=headB;
+//
+//                int t1=0,t2=0;
+//                while(temp1!=null){
+//                    t1++;
+//                    temp1=temp1.next;
+//                }
+//                while(temp2!=null){
+//                    t2++;
+//                    temp2=temp2.next;
+//                }
+//                while(t2>t1){
+//                    headB=headB.next;
+//                    t2--;
+//                }
+//
+//                while(t1>t2){
+//                    headA=headA.next;
+//                    t1--;
+//                }
+//
+//                while(headA!=null || headB!=null){
+//                    if (headA==headB){
+//                        return headA;
+//                    }
+//                    headA=headA.next;
+//                    headB=headB.next;
+//                }
+//
+//                return null;
+//            }
+//        }
+    }
+}
---
+++ 15-LinkList/src/com/company/E_08_LinkedListCycle.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class E_08_LinkedListCycle {
+
+    public class Solution {
+        public boolean hasCycle(ListNode head) {
+            ListNode slow=head;
+            ListNode fast=head;
+
+            while(fast!=null && fast.next!=null){
+                slow=slow.next;
+                fast=(fast.next).next;
+                if(slow==fast){
+                    return true;
+                }
+            }
+
+            return false;
+
+        }
+    }
+
+
+}
